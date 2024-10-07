cdd23cf497615dfc69cd7928100b2cf184436a45
prathamesh <prathameshdol196@gmail.com>
2024-10-07 11:46:51 +0530

    set environment variables

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 
 class Config:
     # SECRET_KEY = os.environ.get('SECRET_KEY')
-    SECRET_KEY = "ksh_hsoig_asdhj_8sd215_as56d56"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
